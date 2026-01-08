62d730972e3650060e0914c1c1d615a8fef9db5b
brianlim05 <brianlim05@hotmail.com>
2026-01-08 10:30:37 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+// include the required packages
+const express = require('express');
+const mysql = require('mysql2/promise');
+require ('dotenv').config();
+const port = 3000;
+
+// database config info
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit: 0,
+};
+
+// initilize Express app
+const app = express ();
+// helps app to read JSON
+app.use(express.json());
+
+// starts live server
+app.listen(port, () => {
+    console.log('Server running on port', port);
+});
+
+
+// Example Route: Get all popmarts
+app.get('/allpopmarts', async (req, res) => {
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.popmarts');
+        res.json(rows);
+    } catch (err) { 
+        console.error(err);
+        res.status(500).json({ message: 'Server error for allpopmarts'})
+    }
+});
+
+// Example Route: Create a new popmart
+app.post('/addpopmart', async (req, res) => {
+    const { popmart_name, artist_name, popmart_pic} = req.body;
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        await connection.execute('INSERT INTO popmarts (popmart_name, artist_name, popmart_pic) VALUES (?, ?, ?)', [popmart_name, artist_name, popmart_pic]);
+        res.status(201).json({ message: 'Popmart '+ popmart_name+' added successfully'});
+    } catch (err) {
+        console.error (err);
+        res.status (500).json({ message: 'Server error - could not add popmart '+ popmart_name});
+    }
+});
+
+// Example Route: Edit a new popmart
